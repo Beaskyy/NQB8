5a00aad6a5f80c302c42a0dbc0a3c3d7d2f5c42e
Beaskyy <afeez.akintobi@yahoo.com>
2024-11-14 16:58:28 +0100

    service

--- components/services.tsx
+++ components/services.tsx
@@ -37,7 +37,7 @@ export const Services = () => {
         className="flex flex-col gap-6 mt-[18px]"
       >
         {services.map(({title, content, image}) => (
-          <div className="flex lg:flex-row flex-col gap-12 bg-white" style={{ position: "sticky", top: 0 }}>
+          <div key={title} className="flex lg:flex-row flex-col gap-12 bg-white" style={{ position: "sticky", top: 0 }}>
           <Image
             src={image}
             alt="service"
@@ -56,39 +56,6 @@ export const Services = () => {
         </div>
         ))}
       </div>
-      {/* <div className="flex flex-col gap-7">
-        {sections.map((section, index) => (
-          <div
-            key={index}
-            className="flex flex-col justify-between bg-white w-full lg:w-[1032px] lg:px-[67px] px-5 py-7 rounded-[22px] lg:flex-row items-center"
-            style={{ position: "sticky", top: 0 }}
-          >
-            <div>
-              <h3 className="mb-[26px] text-[32px] text-black font-semibold tracking-[-0.32] w-[350px] leading-[43px]">
-                {section.title}
-              </h3>
-              <div className="max-w-[388px]">
-                <p className="mb-[26px] text-[15px] text-[#424242]">
-                  {section.description1}
-                </p>
-                <p className="text-[15px] text-[#424242] mb-8">
-                  {section.description2}
-                </p>
-                <div className="flex gap-3 pb-7"></div>
-              </div>
-            </div>
-            <div>
-              <Image
-                src={section.imageSrc}
-                alt={section.imageAlt}
-                width={section.imageWidth}
-                height={section.imageHeight}
-                className="mt-3"
-              />
-            </div>
-          </div>
-        ))}
-      </div> */}
     </div>
   );
 };

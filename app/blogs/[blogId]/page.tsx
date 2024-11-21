be000efc5819150ec3d6145e5d2927fb412e9543
Beaskyy <afeez.akintobi@yahoo.com>
2024-11-21 17:53:29 +0100

    blog page

---
+++ app/blogs/[blogId]/page.tsx
@@ -0,0 +1,159 @@
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
+import Image from "next/image";
+
+const SingleBlog = () => {
+  return (
+    <main>
+      <Header />
+      <div className="lg:px-[148px] md:px-[100px] px-5">
+        <div className="flex md:flex-row flex-col justify-between mt-[52px]">
+          <div className="flex flex-col justify-between max-w-[552px] h-[381px] py-10">
+            <h2 className="text-4xl leading-[53px]">
+              How Automating Your Invoice Process Can Boost Business Efficiency
+            </h2>
+            <div className="flex items-center gap-5">
+              <p>Share</p>
+              <div className="flex gap-2.5">
+                <div className="flex justify-center items-center bg-[#0217200D] w-10 h-10 rounded">
+                  <Image
+                    src="/linkedin.svg"
+                    alt="linkedin"
+                    width={24}
+                    height={24}
+                  />
+                </div>
+                <div className="flex justify-center items-center bg-[#0217200D] w-10 h-10 rounded">
+                  <Image src="/x.svg" alt="x" width={18} height={18} />
+                </div>
+                <div className="flex justify-center items-center bg-[#0217200D] w-10 h-10 rounded">
+                  <Image
+                    src="/facebook.svg"
+                    alt="facebook"
+                    width={20}
+                    height={20}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex bg-[url('/blog-1.png')] bg-cover bg-center bg-no-repeat w-full md:h-[479px] h-[300px]"></div>
+        </div>
+        <div className="flex flex-col gap-[50px] mt-28">
+          <div className="flex flex-col gap-5">
+            <h2 className="text-2xl font-medium leading-[27.36px] tracking-tight">
+              Lorem ipsum dolor sit amet
+            </h2>
+            <p className="text-base text-[#343434] leading-7">
+              Lorem ipsum dolor sit amet consectetur. Ac dignissim in mi sed.
+              Ante semper eros ornare porttitor maecenas dolor porttitor
+              natoque. Condimentum elit tincidunt ac erat phasellus volutpat non
+              pretium morbi. Sapien sed risus libero et eu. Enim ac nunc nulla
+              purus. Nulla elementum condimentum in condimentum aliquet. Iaculis
+              massa sed adipiscing volutpat. Eget diam accumsan proin et
+              adipiscing molestie at id lorem. Luctus vivamus convallis aenean
+              duis. Tortor risus aenean molestie sed lectus leo urna. Mollis mi
+              placerat fringilla faucibus vestibulum eleifend. Enim consectetur
+              integer enim lacus. Ultrices.
+            </p>
+          </div>
+          <div className="flex flex-col gap-5">
+            <h2 className="text-2xl font-medium leading-[27.36px] tracking-tight">
+              Lorem ipsum dolor sit amet
+            </h2>
+            <p className="text-base text-[#343434] leading-7">
+              Lorem ipsum dolor sit amet consectetur. Ac dignissim in mi sed.
+              Lorem ipsum dolor sit amet consectetur. Hendrerit quis et auctor
+              tincidunt eget venenatis sed amet a. Lorem lacus ridiculus risus
+              adipiscing mauris sit tortor ut senectus. Iaculis turpis malesuada
+              mauris sit eros viverra id ut. Id tellus ultrices blandit habitant
+              sed mauris orci. At id sit ut quis volutpat nisl mauris elit.
+              Maecenas sed leo luctus enim pulvinar.
+            </p>
+            <p className="text-base text-[#343434] leading-7">
+              Vitae tortor cursus turpis sed eu. Porttitor phasellus integer
+              varius ac mauris vitae. Posuere scelerisque massa blandit magna
+              fringilla lectus. Sed pellentesque nisl commodo sed euismod mauris
+              ac nulla. Neque non quis elementum gravida. Posuere ut etiam
+              interdum massa. Ut est blandit ac nulla pretium tempor cursus amet
+              ante. Pulvinar suscipit sit arcu nascetur scelerisque eu nunc
+              aliquet. Elit ac adipiscing duis in pellentesque senectus ornare
+              non. Elementum posuere mi viverra vulputate.
+            </p>
+            <p className="text-base text-[#343434] leading-7">
+              Sit adipiscing odio pulvinar vel. Sed viverra id sit nunc odio
+              lectus felis. Senectus quis volutpat metus enim in sed. Lacus nec
+              bibendum nulla non eleifend lacus commodo eget non. Sollicitudin
+              cursus mollis purus scelerisque. Dui aliquam vestibulum
+              consectetur commodo sem amet lacus sit. Dolor et eu auctor in
+              scelerisque aenean pretium dui. Tincidunt luctus ullamcorper diam
+              cras mauris risus. Arcu at nisl pellentesque morbi quisque
+              accumsan nulla hendrerit adipiscing. In pellentesque cras risus
+              aliquam morbi eu. Sit turpis amet diam nunc. Condimentum semper eu
+              sollicitudin tincidunt elementum in varius. Purus eu nam faucibus
+              cras. Viverra habitasse feugiat et elit habitasse. Tortor integer
+              curabitur ullamcorper egestas quis duis. Ut commodo arcu sed
+              congue aenean. Gravida integer bibendum imperdiet porttitor sit
+              ut. Aliquam orci sed volutpat et venenatis a malesuada nulla.
+            </p>
+            <p className="text-base text-[#343434] leading-7">
+              Velit sapien tortor id tempor. Semper venenatis ut cursus volutpat
+              nisl convallis ut. Morbi nisi vel dictum nisl. In proin blandit et
+              dui. Auctor eget arcu id leo. Fermentum turpis magna netus non
+              risus nunc aenean. Donec fringilla ac volutpat ornare gravida
+              mauris feugiat velit. Pharetra a consequat id elit nunc amet.
+              Vitae nisl aliquet in non ornare. Id egestas mauris rhoncus et a
+              nibh feugiat proin aliquet. In lacus amet non turpis consectetur
+              pulvinar. Hac aliquet sit placerat est. Volutpat nibh proin
+              euismod faucibus nulla arcu fusce.
+            </p>
+            <p className="text-base text-[#343434] leading-7">
+              Eu pharetra tristique pellentesque sit at vitae pellentesque.
+              Commodo purus nam id id eros condimentum mauris velit. Proin
+              pellentesque vitae nibh justo sagittis amet enim posuere ut.
+              Feugiat ullamcorper proin non feugiat massa pulvinar congue amet
+              risus. Curabitur pellentesque quam viverra blandit sit. Id lectus
+              tellus ipsum adipiscing consectetur ut nibh. At nisi amet
+              scelerisque a nisl fermentum. Faucibus praesent lacus tincidunt
+              laoreet quis augue. In eu sapien pharetra eu vestibulum integer
+              congue bibendum. Aliquam viverra leo pellentesque pulvinar tempor
+              faucibus. Imperdiet iaculis semper id turpis in eleifend. Turpis
+              commodo euismod dolor lobortis vel lobortis dis auctor. Pharetra
+              risus porttitor nam pharetra. Nam in magna dignissim tincidunt.
+            </p>
+            <p className="text-base text-[#343434] leading-7">
+              Id dui vulputate bibendum duis. Tincidunt ut viverra tristique sit
+              montes ac quis. Molestie lorem pharetra donec arcu nunc. At in ut
+              purus integer. Elementum vitae dignissim volutpat augue
+              sollicitudin nibh donec in. Eu eros vel interdum neque. Venenatis
+              turpis tortor lorem nulla aliquet ipsum. Euismod consectetur amet
+              risus magna pharetra. In malesuada pellentesque dignissim aliquam
+              rhoncus malesuada elementum. At magnis proin elit faucibus odio
+              volutpat eleifend habitasse tempor. Quam lectus mauris id faucibus
+              curabitur et metus bibendum. Donec euismod mus sit nisl. Vitae
+              turpis venenatis lobortis ac neque donec.
+            </p>
+          </div>
+          <div className="flex flex-col gap-5">
+            <h2 className="text-2xl font-medium leading-[27.36px] tracking-tight">
+              Lorem ipsum dolor sit amet
+            </h2>
+            <p className="text-base text-[#343434] leading-7">
+              Lorem ipsum dolor sit amet consectetur. Morbi blandit in aliquam
+              elementum. Nunc amet sed auctor arcu nibh. Sit vulputate elit
+              feugiat sed fermentum. Amet turpis sed leo auctor lectus. Urna
+              facilisis ut sed ut nibh sapien sed. Quam et cum enim proin at
+              tellus in venenatis quis. Blandit arcu dolor elit sit at neque
+              consectetur. Aliquam sem commodo velit posuere massa faucibus
+              purus aenean. Sem in laoreet magna tristique erat faucibus ac.
+              Ornare dignissim tristique enim turpis ipsum.
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </main>
+  );
+};
+
+export default SingleBlog;
